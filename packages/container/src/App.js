8015e3e317443a3af742f15dfdcd3d943365d459
Júlio César <julio.souzam@gmail.com>
2023-11-18 17:04:53 -0300

    feat: change label

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <div>
-      <h1>Hello, Boss</h1>
+      <h1>Hi, Boss</h1>
       <hr />
       <MarketingApp />
     </div>
