8d6bc633827b47095d922656987492590e1c65f7
Júlio César <julio.souzam@gmail.com>
2023-11-19 18:59:20 -0300

    feat: added auth frotnend

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -4,7 +4,7 @@ import { createMemoryHistory, createBrowserHistory } from "history";
 
 import App from "./App";
 
-const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
+const mount = (el, { onSignIn, onNavigate, defaultHistory, initialPath }) => {
   const history =
     defaultHistory ||
     createMemoryHistory({
@@ -12,7 +12,7 @@ const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
     });
   if (onNavigate) history.listen(onNavigate);
 
-  ReactDOM.render(<App history={history} />, el);
+  ReactDOM.render(<App {...{ onSignIn, history }} />, el);
 
   return {
     onParentNavigate: ({ pathname: nextPathname }) => {
@@ -23,7 +23,7 @@ const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
 };
 
 if (process.env.NODE_ENV === "development") {
-  const element = document.querySelector("#_mkt-dev-root");
+  const element = document.querySelector("#_auth-dev-root");
   if (element) {
     mount(element, { defaultHistory: createBrowserHistory() });
   }
